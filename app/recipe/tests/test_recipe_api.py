58c1962dcc88e4ff5d2bd658164b6bef88def7a8
Ryan Knepp <ryan@mirus.io>
2020-11-04 13:33:51 -0600

    added recipe detail view

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -4,11 +4,16 @@ from django.test import TestCase
 
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse("recipe:recipe-list")
+
+
+def detail_url(recipe_id):
+    """return recipe detail url"""
+    return reverse("recipe:recipe-detail", args=[recipe_id])
 
 
 def sample_recipe(user, **params):
@@ -17,6 +22,14 @@ def sample_recipe(user, **params):
     defaults.update(params)
 
     return Recipe.objects.create(user=user, **defaults)
+
+
+def sample_tag(user, name="Sample"):
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name="Bacon"):
+    return Ingredient.objects.create(user=user, name=name)
 
 
 class PublicRecipeApiTests(TestCase):
@@ -61,4 +74,16 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+
         self.assertEqual(res.data, serializer.data)
